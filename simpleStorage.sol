8767299b5ebac06386729df9a55ea03b3e20fd96
CollinsEzeokeke <collinsmecosy@gmail.com>
2025-08-21 09:19:52 +0100

    Trying to configure project to work with zksync

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30; // stating our version
+pragma solidity ^0.8.18; // stating our version
 
 contract SimpleStorage {
     uint256 myFavoriteNumber; // same as 0
