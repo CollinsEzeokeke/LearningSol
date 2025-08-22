dc40cc5b8f24d26846034d15f5c0a1977916c64d
CollinsEzeokeke <collinsmecosy@gmail.com>
2025-08-22 08:09:11 +0100

    deployed a contract from another contract facinating

--- contracts/storageFactory.sol
+++ contracts/storageFactory.sol
@@ -1,10 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+contract SimpleStorage {
+    uint256 myFavoriteNumber; // same as 0
+
+    //uint256[] listOfFavoriteNumbers; // [arrays]
+
+    struct person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // dynamic array
+    person[] public people; // defaults to being empty []
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    // view, pure
+    function retrieve() public view returns(uint256) {
+        return myFavoriteNumber;
+    }
+
+    // callData, memory, storage
+    function addPersons(string memory _name, uint256 _favoriteNumber) public {
+        people.push(person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
+
+
 contract storageFactory {
 
+    SimpleStorage public simpleStorage;
+
     function createSimpleStorageContract() public {
-        
+        simpleStorage = new SimpleStorage();
     }
 
 
